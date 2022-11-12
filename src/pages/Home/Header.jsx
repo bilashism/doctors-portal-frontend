c31918e6b1fcad852c2020bef5bffd8dd95363fd
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-12 18:22:02 +0600

    added Header

--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -1,10 +1,59 @@
 import React from "react";
+import headerImg from "../../images/chair.png";
+import headerBgImg from "../../images/bg.png";
 
 const Header = () => {
   return (
     <header className="">
       <div className="container mx-auto px-4">
-        <h2 className="">Header</h2>
+        <div className="hero bg-base-200 py-16 relative isolate">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <img
+              src={headerImg}
+              className="lg:max-w-xl rounded-lg shadow-2xl"
+              alt="loading"
+              width="904"
+              height="541"
+              decoding="async"
+              fetchpriority="low"
+              loading="lazy"
+            />
+            <div>
+              <h1 className="text-5xl font-bold">
+                Your New Smile Starts Here!
+              </h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button
+                className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-black"
+                type="button">
+                Get Started
+              </button>
+            </div>
+          </div>
+
+          {/* bg image */}
+          <div className="absolute w-full h-full z-[-1] overflow-hidden top-0">
+            <figure className="">
+              <picture>
+                <source srcSet={headerBgImg} />
+                <img
+                  src={headerBgImg}
+                  alt="chair background"
+                  className=""
+                  width="904"
+                  height="541"
+                  decoding="async"
+                  fetchpriority="low"
+                  loading="lazy"
+                />
+              </picture>
+            </figure>
+          </div>
+        </div>
       </div>
     </header>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {}
   },
+  daisyui: {
+    themes: [
+      {
+        doctorsPortal: {
+          primary: "#ead112",
+          secondary: "#f287f2",
+          accent: "#5949b7",
+          neutral: "#302833",
+          "base-100": "#F9F5FA",
+          info: "#78C6E8",
+          success: "#5DDFC3",
+          warning: "#D27C14",
+          error: "#F04628"
+        }
+      }
+    ]
+  },
   plugins: [require("daisyui")]
 };
