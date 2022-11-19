a3344a68458f554e8d91b8968e2b0bd62e35c7f3
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-19 17:58:06 +0600

    create an admin

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,10 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import LoadingCircle from "../../../components/ui/LoadingCircle";
 import { APP_SERVER } from "../../../utilities/utilities";
 import UserRow from "./UserRow";
 
 const AllUsers = () => {
-  const { data: allUsers = [], isLoading } = useQuery({
+  const {
+    data: allUsers = [],
+    isLoading,
+    refetch
+  } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const response = await fetch(`${APP_SERVER}/users`);
@@ -17,26 +22,35 @@ const AllUsers = () => {
     <div>
       <div className="">
         <h2 className="">All Users</h2>
-        <div className="">
-          <div className="overflow-x-auto">
-            <table className="table w-full">
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Email</th>
-                  <th>Name</th>
-                  <th>Hello</th>
-                  <th>Color</th>
-                </tr>
-              </thead>
-              <tbody>
-                {allUsers.map((user, i) => (
-                  <UserRow key={user._id} user={user} idx={i + 1} />
-                ))}
-              </tbody>
-            </table>
+        {isLoading ? (
+          <LoadingCircle />
+        ) : (
+          <div className="">
+            <div className="overflow-x-auto">
+              <table className="table w-full">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Email</th>
+                    <th>Name</th>
+                    <th>Admin</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {allUsers.map((user, i) => (
+                    <UserRow
+                      key={user._id}
+                      user={user}
+                      idx={i + 1}
+                      refetch={refetch}
+                    />
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
--- src/pages/Dashboard/AllUsers/UserRow.jsx
+++ src/pages/Dashboard/AllUsers/UserRow.jsx
@@ -1,14 +1,54 @@
 import React from "react";
+import toast from "react-hot-toast";
+import { APP_SERVER } from "../../../utilities/utilities";
 
-const UserRow = ({ user, idx }) => {
-  const { _id, email, name } = user;
+const UserRow = ({ user, idx, refetch }) => {
+  const { _id, email, name, role } = user;
+
+  const makeAdmin = id => {
+    fetch(`${APP_SERVER}/users/admin/${id}`, {
+      method: "put",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.modifiedCount >= 1) {
+          toast.success(`${name} is an admin now!`);
+          refetch();
+        }
+      })
+      .catch(err => console.error(err));
+  };
+
+  const deleteUser = id => {
+    console.log(id);
+  };
+
   return (
     <tr className="hover">
       <th>{idx}</th>
       <td>{email}</td>
       <td>{name}</td>
-      <td>hello</td>
-      <td>red</td>
+      <td>
+        {role !== "admin" && (
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => makeAdmin(_id)}>
+            make admin
+          </button>
+        )}
+      </td>
+      <td>
+        <button
+          className="btn btn-accent"
+          type="button"
+          onClick={() => deleteUser(_id)}>
+          delete user
+        </button>
+      </td>
     </tr>
   );
 };
